15810ec03729e17145310b441ca7254b4a1e462c
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-13 15:15:06 +0900

    Add button to delete order line

--- source/js/controllers/index_controller.js
+++ source/js/controllers/index_controller.js
@@ -2,6 +2,10 @@ Account.IndexController = Ember.ObjectController.extend({
   actions: {
     addOrderLine: function() {
       this.get('model.lines').addObject(Account.OrderLine.create());
+    },
+
+    deleteLine: function(orderLine) {
+       this.get('model.lines').removeObject(orderLine);
     }
   }
 });
