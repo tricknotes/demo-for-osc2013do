65a8dc98a965fd661a5f4332d4811e9d8c16a9f6
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-14 03:52:33 +0900

    Remove unused

--- source/js/routes/sheet.js
+++ source/js/routes/sheet.js
@@ -1,8 +1,4 @@
 Account.SheetRoute = Ember.Route.extend({
-  model: function(params) {
-    return Account.Sheet.sheets[params.id - 0];
-  },
-
   afterModel: function(model) {
     if (!model) {
       this.transitionTo('not_found');
