067831e647bf1b5a5fcbebe3f50df77719c38a56
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-13 17:40:16 +0900

    WIP: Apply fontawesome

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 gem 'middleman', '~>3.1.4'
 gem 'ember-middleman', github: 'tricknotes/ember-middleman'
+gem 'font-awesome-middleman'
 
 gem 'vendorer'
--- source/js/account.js
+++ source/js/account.js
@@ -2,6 +2,7 @@
 //
 //=require_tree ./models
 //=require_tree ./controllers
+//=require_tree ./components
 //=require_tree ./views
 //=require_tree ./templates
 //=require_tree ./routes
