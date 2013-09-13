5684d04d16d722f5125b9fb2f09372230aac4d96
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-09-13 22:21:43 +0900

    Use 'relative_assets' for gh-pages

--- config.rb
+++ config.rb
@@ -6,10 +6,11 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
+
+  activate :relative_assets
 end
 
 activate :ember
