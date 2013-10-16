6b377f5dde8051dc2cd25b345152017a22dc6f28
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-10-16 23:35:05 +0900

    Update with b4f8a24c

--- js/models/sheet-0d84e62a.js
+++
@@ -1,1 +0,0 @@
-Account.Sheet=Ember.Object.extend({lines:[],createdAt:null,id:function(){return Account.Sheet.sheets.indexOf(this)+1}.property("Account.Sheet.sheets.length"),total:function(){return this.get("lines.@each.subtotal").reduce(function(e,t){return e+(t||0)},0)}.property("lines.@each.subtotal")}),Account.Sheet.sheets=[];
